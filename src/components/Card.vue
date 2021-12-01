27d12d0b99e3041a4c42085379ddf3241b6c5e25
MyUsername <my@email.com>
2021-12-01 12:03:58 +0100

    creazione card

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="card">
+    <h3>Titolo:
+        <span>{{results.title}}</span>
+    </h3>
+    <h3>Titolo originale:
+        <span>{{results.original_title}}</span>
+    </h3>
+    <h3>Lingua:
+        <span>{{results.original_language}}</span>
+    </h3>
+    <h3>Voto:
+        <span>{{results.vote_average}}</span>
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+      details: Object
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,28 @@
 <template>
   <main>
-      
+      <Card v-for="movie, i in results" :key="i" :details="movie"/>
   </main>
 </template>
 
 <script>
+import axios from "axios";
+import Card from './components/Card.vue';
+
 export default {
   name: 'Main',
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=3db9c7a2f859884185a1de6ddc97c03b&query=ritorno+al+futuro",
+    }
+  },
 }
 </script>
 
 <style scoped lang="scss">
-
+main {
+    background-color: #434343;
+}
 </style>
